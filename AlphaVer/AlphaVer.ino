d8bcb345fb18594df4b98004984957859a5b60d4
Andrey Morenkov <jedi-andr@mail.ru>
2016-10-29 04:25:09 +0300

    v 0.6
    - Added Serial Streaming
    - Added developer commands
    - Some fixes

--- AlphaVer/AlphaVer.ino
+++ AlphaVer/AlphaVer.ino
@@ -1,202 +1,72 @@
+
+
 //-------- Initialization --------
 
+#include <Streaming.h>
 #include <TimerOne.h>
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 
+#include "Sensor.h"
+#include "Plant.h"
+
+// цифровые
+unsigned char       *device;                         // Массив цифровых устройств
+int                 Interrupt_pin    = 2;            // Порт прерываний 
 const unsigned char Valve_0          = 3;            // Клапан #1
 const unsigned char Valve_1          = 4;            // Клапан #2
 const unsigned char Pump             = 5;            // Помпа
 const unsigned char Relay_Sensors    = 10;           // Сенсоры
+//
 
+// аналоговые
 const unsigned char WaterSensor      = A0;           // Сенсор уровня воды
 const unsigned char HygroMeter_0     = A1;           // Гигрометр #1
 const unsigned char HygroMeter_1     = A2;           // Гигрометр #2
+//
 
-const int Alert_WS                   = 470;          // Критическое значение WaterSensor     (0 - сухо, 1024 - мокро)
-const int Alert_HM                   = 700;          // Критическое значение HygroMeter      (1024 - сухо, 0 - мокро)
-const int MaxTime_P                  = 5000;         // Максимальное время работы Pump       (в мс)
-const int MaxFlowers                 = 2;            // Количество контроллируемых цветков
-const int MaxSensors                 = MaxFlowers+1; // Количество сенсоров = кол-во цветков + сенсор воды
+const int           Alert_WS         = 470;          // Критическое значение WaterSensor     (0 - сухо, 1024 - мокро)
+const int           Alert_HM         = 700;          // Критическое значение HygroMeter      (1024 - сухо, 0 - мокро)
+const int           PumpTime         = 5000;         // Максимальное время работы Pump       (в мс)
+const int           MaxFlowers       = 2;            // Количество контроллируемых цветков    (сделать динамически через sizeof) !!!!!!!!!!!!!!!!!!!!!!!!
+const int           MaxSensors       = MaxFlowers+1; // Количество сенсоров = кол-во цветков + сенсор воды
+const int           MaxDevices       = MaxFlowers+2; // Количество устройств = кол-во клапанов + помпа + реле сенсоров
 
-const unsigned int Cycle             = 10000;        // Цикл работы (в мс)
-unsigned long      MaxTime           = 4294967295;   // Максимальное время в ардуино (мс)
-unsigned long      CurrTime          = 0;            // Текущее время работы
-unsigned long      StartProcess      = 0;            // Время начала работы процесса
+unsigned long       cycle_time       = 40;           // Цикл проверки датчиков ( в сек) // 86400 = 1 сутки
+
+int                 timer_cycle      = 8;            // Цикл таймера в сек
+
+
+char* command_fragment;
+
+bool IsDev = false;
 
 char number = -1;
-int pin = 2;
+
 String command = "";
-char command_fragment;
+
 
 volatile bool  wakeup_timer = false;
-volatile bool  wakeup_bt = false;
-volatile int  timer_count = 0;
+volatile bool  wakeup_bt    = false;
+volatile int   timer_count  = 0;
+volatile long  cycle        = cycle_time;   // Тут будем декрементить
 
-#define ON LOW
+#define ON  LOW
 #define OFF HIGH
-#define WS MaxSensors-1
+#define WS  MaxSensors-1
+#define PMP MaxDevices-2
+#define RS  MaxDevices-1
+
+Sensor* sensor;                               // Массив сенсоров
+Plant*  flower;                               // Массив цветков
 
 //--------------------------------
 
-void LedOn()
-{
-  digitalWrite(13, HIGH);
-}
+void Timer1_action();
 
-void LedOff()
-{
-  digitalWrite(13, LOW);
-}
-
-//-------------------------------------------------------------SENSOR--------------------------------------------------------------
-class Sensor
-{
-  private:
-  
-      signed char pin;         // пин сенсора
-      int         val;         // показания сенсора  
-      
-  public:
-      
-      Sensor( signed char _pin = -1)
-      {
-          pin = _pin;
-          val = -1;
-      }
-
-      Sensor (const Sensor& _sensor )
-      {
-          pin = _sensor.getPin();
-          val = _sensor.getVal();         //опасно ??
-      }   
-
-      void setPin(signed char _pin)
-      {
-          pin = _pin;
-      }
-
-      signed char getPin () const
-      {
-          return pin;
-      }
-
-      int getVal() const
-      {
-          return val;
-      }
-
-      int ask()
-      {
-          val = analogRead(pin);
-          Serial.print("   Sensor.ask() Value: ");
-          Serial.println(val);
-          return val;
-      }
-};  
-//------------------------------------------------------------/SENSOR--------------------------------------------------------------
-Sensor* sensor;                               // Массив сенсоров
-//--------------------------------------------------------------PLANT--------------------------------------------------------------
-class Plant
-{
-  private:
-  
-      signed char valve;         // пин клапана
-      Sensor      hygrometer;    // гигрометр
-      
-  public:  
-
-      Plant (signed char _valve = -1, Sensor _hygrometer = -1)
-      {
-          valve = _valve;
-          hygrometer = _hygrometer;
-      }
-      
-      Plant (Plant& _flower)
-      {
-          valve =      _flower.getValve();
-          hygrometer = _flower.getHygrometer().getPin();
-      }   
-      
-      void setValve(signed char _valve)
-      {
-          valve = _valve;
-      }
-
-      void setHygrometer(Sensor _hygrometer)
-      {
-          hygrometer = _hygrometer;
-      }
-      
-      void setParams (signed char _valve, Sensor _hygrometer)
-      {
-          valve = _valve;
-          hygrometer = _hygrometer;
-      }
-
-      signed char getValve()
-      {
-          return valve;
-      }
-
-      Sensor getHygrometer()
-      {
-          return hygrometer;
-      }
-
-      int getStat()
-      {
-          return hygrometer.getVal();
-      }
-    
-      void init()
-      {
-          pinMode(valve,OUTPUT);
-      }
-
-      void ask()
-      {
-          Serial.println("   Plant.ask() Asking hygrometer...");
-          hygrometer.ask();
-      }
-
-      void water()
-      {
-          Serial.println("STARTING TO WATER... ");
-          if ( sensor[WS].getVal() <= Alert_WS )                            // Если воды меньше нормы то опрос долива воды
-          {
-              while (sensor[WS].getVal() <= Alert_WS)                       
-              {
-                  Serial.println("NO WATER !");
-                  LedOn();
-                  delay(2000);
-                  Serial.println("ASKING FOR WATER...");
-                  digitalWrite(Relay_Sensors, ON);
-                  delay(500);
-                  sensor[WS].ask();
-                  digitalWrite(Relay_Sensors, OFF);
-              }
-              LedOff();
-          }
-          Serial.println("OK WATER, watering... ");    
-          digitalWrite(valve,ON);
-          Serial.println("VALVE ON");
-          digitalWrite(Pump,ON);
-          Serial.println("PUMP ON");
-          delay(MaxTime_P);
-          digitalWrite(Pump,OFF);
-           Serial.println("PUMP OFF");
-          delay(200);
-          digitalWrite(valve,OFF);
-          Serial.println("VALVE OFF");        
-      }
-                 
-};
-//-------------------------------------------------------------/PLANT--------------------------------------------------------------
-
-Plant*  flower;                               // Массив цветков
-
+//--------------------------------------------------------------SETUP--------------------------------------------------------------
 void setup()
 {
      Serial.begin(9600);                      // Скорость (бит/c) , для отладки
@@ -204,10 +74,13 @@ void setup()
      pinMode(Pump,OUTPUT);                    // Устанавливаем пин Pump
      pinMode(Relay_Sensors, OUTPUT);          // Устанавливаем пин датчиков
         
-     digitalWrite(Pump, OFF);
-     digitalWrite(Valve_0, OFF);
-     digitalWrite(Valve_1, OFF);
-     digitalWrite(Relay_Sensors, OFF);
+     device = new unsigned char[MaxDevices];  // массив для цифровых девайсов
+     device[0]   = Valve_0;
+     device[1]   = Valve_1;
+     device[PMP] = Pump;
+     device[RS]  = Relay_Sensors;
+
+     DevicesOFF();
 
      sensor = new Sensor[MaxSensors];
      sensor[0].setPin(HygroMeter_0);                // Гигрометр #1
@@ -221,17 +94,39 @@ void setup()
 
      for (int i = 0; i < MaxFlowers; i++)
      {
+        // инициализация цветков
         flower[i].init();
      }
      
-     pinMode(pin, INPUT);               // порт прерывания как выход
+     pinMode(Interrupt_pin, INPUT);                  // порт прерывания как выход
   
-     set_sleep_mode(SLEEP_MODE_IDLE);   //Определяем режим сна
+     set_sleep_mode(SLEEP_MODE_IDLE);                // Определяем режим сна
 
-     Timer1.initialize(8000000);        // 8 сек
-     Timer1.attachInterrupt(Timer1_action); // прерывание на таймере должно быть всегда
+     Timer1.initialize(timer_cycle * 1000000);       // x сек
+     Timer1.attachInterrupt(Timer1_action);          // прерывание на таймере должно быть всегда
+
+     IsDev = true; // пока работаем в дев моде всегда
   
      Serial.println("Initialisation complete.");
+}
+//-------------------------------------------------------------/SETUP--------------------------------------------------------------
+
+void LedOn()
+{
+  digitalWrite(13, HIGH);
+}
+
+void LedOff()
+{
+  digitalWrite(13, LOW);
+}
+
+void DevicesOFF()
+{
+    for (int i = 0; i < MaxDevices; i++)
+    {
+      digitalWrite(device[i],OFF);
+    }
 }
 
 void CheckSensors()
@@ -257,14 +152,78 @@ void CheckSensors()
   
 }
 
+void ForceWater(int N)
+{
+  Serial << "    Beginning of watering FLOWER # " << N << " :" << endl;
+  flower[N].water();
+  Serial << "    End of watering FLOWER # " << N << endl;
+}
+
+void WaterPlant(int N)
+{
+  if (flower[N].getStat() > Alert_HM)
+  {
+    ForceWater(N);
+  }
+  else
+  {
+    Serial << "    Don't need to water" << endl;
+  }
+}
+
+
+
+void ParceCommand()
+{
+  while (Serial.available() > 0)
+  {
+    // пока в буфере есть данные, записываем их в строку
+    *command_fragment = Serial.read();
+    command += *command_fragment;      
+  }
+}
+
+void DoDevCommand(String _command)
+{
+  int Vald;
+
+  if (_command.charAt(1) == '0')
+  {
+    IsDev = false;
+    Serial.println("  DevMode OFF");
+  }
+  else
+  {
+    Vald = _command.substring(_command.indexOf('s')+1).toInt();
+    Serial.print("Val = ");
+    Serial.println(Vald);
+     
+    if (_command.charAt(1) == 'f')
+    {
+      Serial.print("Target: flower ");
+      Serial.println(_command.charAt(2));
+      flower[_command.charAt(2)-'0'].setVal(Vald);
+      flower[_command.charAt(2)-'0'].ask();
+    }
+    if (_command.charAt(1) == 'w')
+    {
+      Serial.print("Target: WS");
+      sensor[WS].setVal(Vald);
+      sensor[WS].ask();
+    }      
+    Serial.println("DevMode ON");
+    Serial.println("Print d0 to OFF");
+  }      
+}
+
+
 void Timer1_action()
 {
-  timer_count++;
-
-  // 40 сек
-  if (timer_count == 5)
+  cycle -= timer_cycle;
+  if (cycle <= 0)
   {
     wakeup_timer = true;
+    cycle = cycle_time;    // возобновили счетчик таймера
   }
 }
 
@@ -275,8 +234,8 @@ void BT_Action()
 
 void EnterSleep()
 {
-  sleep_enable(); //Разрешаем спящий режим
-  attachInterrupt(INT0, BT_Action, LOW); //Если на 0-вом прерываниии - ноль, то просыпаемся.
+  sleep_enable();                                  //Разрешаем спящий режим
+  attachInterrupt(INT0, BT_Action, LOW);  //Если на 0-вом прерываниии - ноль, то просыпаемся.
 
   // выключаем ненужные модули
   power_adc_disable();
@@ -287,114 +246,112 @@ void EnterSleep()
   
     
   sleep_mode();
-  //---------------------------------
-  // СОН
-  //---------------------------------
+  // тут сон
   
-  sleep_disable();    // Запрещаем спящий режим
-  power_all_enable(); // Врубаем все
-  detachInterrupt(0); // Отключаем прерывания по BT
+  sleep_disable();                // Запрещаем спящий режим
 
-  Serial.println("WakeUp"); //Проснулись
-  delay(80); // ждем пока все проснется
+  // Врубаем все
+  power_twi_enable();
+  power_timer2_enable();
+  power_timer0_enable();
+  power_spi_enable();
+  power_adc_enable();               
+               
+  detachInterrupt(INT0);          // Отключаем прерывания по BT
+
+  Serial.println("WakeUp");       // Проснулись
+  delay(80);                      // Ждем пока все проснется
 }
 
-
+//-------------------------------------------------------------LOOP--------------------------------------------------------------
 void loop()
 {
-  Serial.println("--Beggining of cycle--");
+  Serial << "-- Cycle Start --" << endl;
   delay(50); 
-
-  // sensors off
-  digitalWrite(Pump, OFF);
-  digitalWrite(Valve_0, OFF);
-  digitalWrite(Valve_1, OFF);
-  digitalWrite(Relay_Sensors, OFF);
-     
-  EnterSleep();        //Подготовка ко сну и сон
-  // после сна начинаем отсюда
   
+  DevicesOFF();       // на всякий случай принудительно все выключим
+  if (!wakeup_timer)
+  {
+    EnterSleep();
+  }   
+  //----------- СОН -----------
+  interrupts();
+  Serial << "Cycle = " << cycle << endl;
   if (wakeup_timer)
   {
-    Serial.println("WAKEUP from timer (40 sec)");
+    Serial << "wakeup from TIMER (" << timer_cycle <<" sec)"<<endl;
     wakeup_timer = false;
-    timer_count = 0;
+    
     CheckSensors();
     delay(100);
     for (int i = 0; i < MaxFlowers; i++)
     {
-        
-        Serial.print("Curr / Alert :");
-        Serial.print(flower[i].getStat());
-        Serial.print(" / ");
-        Serial.println(Alert_HM);
-        
-        if (flower[i].getStat() > Alert_HM)
-        {
-            Serial.print("Beginning of watering FLOWER #");
-            Serial.print(i);
-            Serial.println(" :");
-            flower[i].water();
-            Serial.print("End of watering FLOWER #");
-            Serial.print(i);
-            Serial.println(" :");
-        }
+      Serial << "Curr/Alert: " << flower[i].getStat() <<"/"<< Alert_HM << endl;
+      WaterPlant(i);
     }
-    digitalWrite(Pump, OFF);
-    digitalWrite(Valve_0, OFF);
-    digitalWrite(Valve_1, OFF);
-    digitalWrite(Relay_Sensors, OFF);
-
-    Serial.println("--End of cycle, going to sleep--");
-    Serial.println("");
-    Serial.println("");
+    DevicesOFF();
+    Serial << "-- Cycle End--" << endl;
+    Serial << "" << endl;
+    Serial << "" << endl;
   }
   
   if (wakeup_bt)
   {
-    Serial.println("WAKEUP from BT"); 
+    Serial << "wakeup from BLUETOOTH" << endl;
     wakeup_bt = false;
+    
+    command_fragment = new char[1];
+    ParceCommand();
+    delete[] command_fragment;
 
-    while (Serial.available() > 0)
-    {
-      // пока в буфере есть данные, записываем их в строку
-      command_fragment = Serial.read();
-      command += command_fragment;      
-    }
     Serial.print("Incoming command: ");
-    Serial.println(command.toInt());
-    if (command.toInt() == 1)
+    Serial.println(command);
+    
+    if (command.startsWith("d"))
     {
-      // для теста чекнуть сенсоры
-      CheckSensors();
+      // developer mode
+      IsDev = true;
+      DoDevCommand(command);
     }
-    if (command.toInt() == 2)
+    else
     {
-      for (int i = 0; i < MaxFlowers; i++)
-    {
-        
-        Serial.print("Curr / Alert :");
-        Serial.print(flower[i].getStat());
-        Serial.print(" / ");
-        Serial.println(Alert_HM);
-        
-        if (flower[i].getStat() > Alert_HM)
+      if (command.toInt() == 1)
+      {
+        // для теста чекнуть сенсоры
+        CheckSensors();
+      }
+      if (command.toInt() == 2)
+      {
+        // полить принудительно
+        for (int i = 0; i < MaxFlowers; i++)
         {
-            Serial.print("Beginning of watering FLOWER #");
-            Serial.print(i);
-            Serial.println(" :");
-            flower[i].water();
-            Serial.print("End of watering FLOWER #");
-            Serial.print(i);
-            Serial.println(" :");
+          ForceWater(i);
         }
-    }
-    digitalWrite(Pump, OFF);
-    digitalWrite(Valve_0, OFF);
-    digitalWrite(Valve_1, OFF);
-    digitalWrite(Relay_Sensors, OFF);
+      }
+      DevicesOFF();
     }
     command = "";
   }
 }
+//------------------------------------------------------------/LOOP--------------------------------------------------------------
+
+/* Полезное
+ * 
+ *  Для отладки
+ *  //char buf[24];
+ *  //snprintf(buf, sizeof(buf), "flower: 0x%x", flower);
+ *  //Serial.println(buf);
+ * 
+ *  Старая версия выкл все
+ *  //digitalWrite(Pump, OFF);
+ *  //digitalWrite(Valve_0, OFF);
+ *  //digitalWrite(Valve_1, OFF);
+ *  //digitalWrite(Relay_Sensors, OFF);
+ * 
+ * 
+ */
+
+
+
+
 
---
+++ AlphaVer/Sensor.h
@@ -0,0 +1,67 @@
+#pragma once
+
+extern bool IsDev;
+
+
+//-------------------------------------------------------------SENSOR--------------------------------------------------------------
+class Sensor
+{
+  private:
+  
+      signed char pin;         // пин сенсора
+      int         val;         // показания сенсора  
+      
+  public:
+      
+      Sensor( signed char _pin = -1)
+      {
+          pin = _pin;
+          val = -1;
+      }
+
+      Sensor (const Sensor& _sensor )
+      {
+          pin = _sensor.getPin();
+          val = _sensor.getVal();        
+      }   
+
+      void setPin(signed char _pin)
+      {
+          pin = _pin;
+      }
+
+      signed char getPin () const
+      {
+          return pin;
+      }
+
+      int getVal() const
+      {
+          return val;
+      }
+
+      void setVal(int _val)
+      {
+        val = _val;
+      }
+
+      int ask()
+      {
+        if (!IsDev)
+        {
+          val = analogRead(pin);
+          Serial.print("   Sensor.ask() Value: ");
+          Serial.println(val);
+          return val;
+        }
+        else
+        {
+          Serial.println("  DEV MODE");
+          Serial.print("   Sensor.ask() Value: ");
+          Serial.println(val);
+          return val;
+        }
+          
+      }
+};  
+//------------------------------------------------------------/SENSOR--------------------------------------------------------------
